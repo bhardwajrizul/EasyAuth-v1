8b0ab98f68b9c9125575392ad15356db6f53ce17
bhardwajrizul <bhardwajrizul9816@gamil.com>
2022-10-07 20:02:37 +0530

    update port env variable

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ async function insertADoc() {
 
 // host and port of our local server
 const host = '127.0.0.1';
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // REGEX EMAIL
 const emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
